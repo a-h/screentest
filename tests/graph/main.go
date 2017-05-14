2ad241528ffa0baf45ae0ab47d9c00a550668055
Adrian Hesketh <adrianhesketh@hushmail.com>
2017-05-14 21:13:12 +0100

    Migrated useful code to external packages.

--- tests/graph/main.go
+++ tests/graph/main.go
@@ -5,6 +5,9 @@ import (
 	"image"
 	"image/color"
 
+	"github.com/a-h/raster"
+	"github.com/a-h/scaler"
+
 	"math"
 
 	"golang.org/x/exp/shiny/driver"
@@ -39,65 +42,59 @@ func main() {
 
 		border := 50
 
-		xScale := Scaler{
-			InputMinimum:  0 + border,
-			InputMaximum:  bounds.Width() - border,
-			OutputMinimum: -10,
-			OutputMaximum: +10,
-		}
+		xScaler := scaler.New(
+			0+border,              // Input Minimum
+			bounds.Width()-border, // Input Maximum
+			-10, // Output Minimum
+			+10) // Output Maximum
 
-		yScale := Scaler{
-			InputMinimum:  -1,
-			InputMaximum:  +1,
-			OutputMinimum: bounds.Height() - border,
-			OutputMaximum: 0 + border,
-		}
+		yScaler := scaler.New(-1, +1, bounds.Height()-border, 0+border)
 
 		// Draw the x-axis.
 		for ix := 0; ix < bounds.Width(); ix++ {
 			x := ix
-			y := yScale.Calculate(0)
+			y, _ := yScaler.Scale(0)
 			img.Set(x, int(y), axesColor)
 		}
 
 		// Draw the y-axis.
 		for iy := 0; iy < bounds.Height(); iy++ {
-			x := xScale.CalculateInverse(0)
+			x, _ := xScaler.Invert(0)
 			y := iy
 			img.Set(int(x), y, axesColor)
 		}
 
 		for ix := 0; ix < bounds.Width(); ix++ {
 			// Draw htan.
-			x := xScale.Calculate(float64(ix))
+			x, _ := xScaler.Scale(float64(ix))
 			y := htan(x)
-			iy := int(yScale.Calculate(y))
-			img.Set(ix, iy, colorA)
+			iy, _ := yScaler.Scale(y)
+			img.Set(ix, int(iy), colorA)
 
 			// Draw sigmoid.
-			x = xScale.Calculate(float64(ix))
+			x, _ = xScaler.Scale(float64(ix))
 			y = sigmoid(x)
-			iy = int(yScale.Calculate(y))
-			img.Set(ix, iy, colorB)
+			iy, _ = yScaler.Scale(y)
+			img.Set(ix, int(iy), colorB)
 		}
 
 		// Middle to top right.
-		Line(img, 900, 500, 1800, 0, color.RGBA{R: 0xFF, G: 0x00, B: 0x00})
+		raster.DrawLine(img, 900, 500, 1800, 0, color.RGBA{R: 0xFF, G: 0x00, B: 0x00})
 		// Middle to bottom right.
-		Line(img, 900, 500, 1800, 1000, color.RGBA{R: 0x00, G: 0xFF, B: 0x00})
+		raster.DrawLine(img, 900, 500, 1800, 1000, color.RGBA{R: 0x00, G: 0xFF, B: 0x00})
 		// Middle to top left.
-		Line(img, 900, 500, 0, 0, color.RGBA{R: 0x00, G: 0x00, B: 0xFF})
+		raster.DrawLine(img, 900, 500, 0, 0, color.RGBA{R: 0x00, G: 0x00, B: 0xFF})
 		//Middle to bottom left.
-		Line(img, 900, 500, 0, 1000, color.RGBA{R: 0xFF, G: 0xFF, B: 0xFF})
+		raster.DrawLine(img, 900, 500, 0, 1000, color.RGBA{R: 0xFF, G: 0xFF, B: 0xFF})
 
 		// Top to bottom.
-		// Line(img, 30, 0, 30, 1800, color.RGBA{R: 0xFF, G: 0xFF, B: 0xFF})
+		// raster.Line(img, 30, 0, 30, 1800, color.RGBA{R: 0xFF, G: 0xFF, B: 0xFF})
 		// Left to right.
-		// Line(img, 0, 30, 1800, 30, color.RGBA{R: 0xFF, G: 0xFF, B: 0xFF})
+		// raster.Line(img, 0, 30, 1800, 30, color.RGBA{R: 0xFF, G: 0xFF, B: 0xFF})
 
-		Circle(img, 900, 500, 60, color.RGBA{R: 0xFF, G: 0x00, B: 0x00})
+		raster.DrawCircle(img, 900, 500, 60, color.RGBA{R: 0xFF, G: 0x00, B: 0x00})
 
-		Disc(img, 1200, 700, 60, color.RGBA{R: 0x00, G: 0x33, B: 0x00})
+		raster.DrawDisc(img, 1200, 700, 60, color.RGBA{R: 0x00, G: 0x33, B: 0x00})
 
 		w.Upload(image.Point{0, 0}, background, image.Rect(0, 0, bounds.Width(), bounds.Height()))
 		w.Publish()
@@ -118,27 +115,6 @@ func main() {
 			}
 		}
 	})
-}
-
-type Scaler struct {
-	InputMinimum  int
-	InputMaximum  int
-	OutputMinimum int
-	OutputMaximum int
-}
-
-func (s Scaler) Calculate(in float64) float64 {
-	outputRange := float64(s.OutputMaximum - s.OutputMinimum)
-	inputRange := float64(s.InputMaximum - s.InputMinimum)
-
-	return ((outputRange / inputRange) * (in - float64(s.InputMinimum))) + float64(s.OutputMinimum)
-}
-
-func (s Scaler) CalculateInverse(in float64) float64 {
-	outputRange := float64(s.InputMaximum - s.InputMinimum)
-	inputRange := float64(s.OutputMaximum - s.OutputMinimum)
-
-	return ((outputRange / inputRange) * (in - float64(s.OutputMinimum))) + float64(s.InputMinimum)
 }
 
 func htan(x float64) float64 {
@@ -180,72 +156,4 @@ func (b Bounds) Height() int {
 type WidthHeight interface {
 	Width() int
 	Height() int
-}
-
-func Circle(img *image.RGBA, x, y int, radius int, c color.RGBA) {
-	bounds := image.Rect(x-radius-2, y-radius-2, x+radius+2, y+radius+2)
-	for ix := bounds.Min.X; ix < bounds.Max.X; ix++ {
-		for iy := bounds.Min.Y; iy < bounds.Max.Y; iy++ {
-			width := x - ix
-			height := y - iy
-
-			distanceFromCenter := math.Sqrt(float64(((width * width) + (height * height))))
-			if int(distanceFromCenter) == radius {
-				img.Set(ix, iy, c)
-			}
-		}
-	}
-}
-
-func Disc(img *image.RGBA, x, y int, radius int, c color.RGBA) {
-	bounds := image.Rect(x-radius-2, y-radius-2, x+radius+2, y+radius+2)
-	for ix := bounds.Min.X; ix < bounds.Max.X; ix++ {
-		for iy := bounds.Min.Y; iy < bounds.Max.Y; iy++ {
-			width := x - ix
-			height := y - iy
-
-			distanceFromCenter := math.Sqrt(float64(((width * width) + (height * height))))
-			if int(distanceFromCenter) <= radius {
-				img.Set(ix, iy, c)
-			}
-		}
-	}
-}
-
-func Line(img *image.RGBA, fromX, fromY int, toX, toY int, c color.RGBA) {
-	// Vertical line.
-	if fromX == toX {
-		for y := fromY; y < toY; y++ {
-			img.Set(fromX, y, c)
-		}
-		return
-	}
-
-	// Horizontal line.
-	if fromY == toY {
-		for x := fromX; x < toX; x++ {
-			img.Set(x, fromY, c)
-		}
-		return
-	}
-
-	// It's a slope.
-	// We're moving from fromX to toX, so make sure they're in the right order.
-	if toX < fromX {
-		toX, toY, fromX, fromY = fromX, fromY, toX, toY
-	}
-
-	var b int
-	if toY < fromY {
-		b = img.Bounds().Dy()
-	}
-
-	rise := toY - fromY
-	run := toX - fromX
-	m := float64(rise) / float64(run)
-
-	for x := fromX; x <= toX; x++ {
-		y := (m * float64(x)) + float64(b)
-		img.Set(x, int(y), c)
-	}
 }
